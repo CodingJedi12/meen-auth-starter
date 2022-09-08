e958a3edc3684ac1121f39e15c6dacbacedc1cba
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-08 15:25:04 -0400

    added controllers, views and connected them to main

---
+++ controllers/sessions.js
@@ -0,0 +1,49 @@
+// Dependencies
+const express = require('express');
+const bcrypt = require('bcrypt');
+const sessionsRouter = express.Router();
+const User = require('../models/user.js');
+
+// New (login page)
+sessionsRouter.get('/new', (req, res) => {
+	res.render('sessions/new.ejs')
+})
+
+// Delete (logout route)
+sessionsRouter.delete('/', (req, res) => {
+    req.session.destroy((error) => {
+        res.redirect('/');
+    })
+})
+
+// Create (login route)
+sessionsRouter.post('/', (req, res) => {
+    //check for existing user
+    User.findOne({
+        email: req.body.email,
+    }, (error, foundUser) => {
+        //sends a message saying the user email is not in the database
+        if (!foundUser) {
+            res.send(`Oops! We do not have a user with that email address.`);
+        }
+        //compares the given password with the hashed password stored
+        if (foundUser) {
+            const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password); //compares the password entered in the body with the password linked to the one stored at "foundUser"
+
+            //if the password matches, we add the user to the session
+            if (passwordMatches) {
+                req.session.currentUser = foundUser;
+
+                //redorect the user back to the home page
+                res.redirect('/');
+            }
+            //if the passwords do not match
+            if (!passwordMatches) {
+                res.send("Oops! The credentials entered are invalid..")
+            }
+        }
+    });
+});
+
+// Export Sessions Router
+module.exports = sessionsRouter;
---
+++ server.js
@@ -0,0 +1,43 @@
+// Dependencies
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+require('dotenv').config();
+
+//Database Connection
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+// Session middleware
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// Routes / Controllers
+app.get('/', (req, res) => {
+	res.render('index.ejs');
+});
+const userController = require('./controllers/users');
+app.use('/users', userController);
+const sessionsController = require('./controllers/sessions');
+app.use('/sessions', sessionsController);
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
